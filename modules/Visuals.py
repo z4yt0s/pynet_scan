1b9f948dc62752a44ba694c8c70692d5acc0a9d8
Zaytos <zaytos@localhost.localdomain>
2023-12-09 20:53:07 +0100

    adding banner and optimization colorized_print

--- modules/Visuals.py
+++ modules/Visuals.py
@@ -1,5 +1,7 @@
+from time import sleep
 from rich.console import Console
 from rich.theme import Theme
+from rich.text import Text
 
 """
 Visuals: is in charge of setting, preparing and rendering all the visual elements of 
@@ -20,16 +22,39 @@ class Visuals:
         #if type_msg == 'default':
             #self.type_msg = Visuals.type_msg
         self.console = Console(color_system='256', theme=Theme(Visuals.type_msg))
+        self.text = Text()
+
     """
     This print the banner of the tool
     """
-    def banner():
-        pass
+    def banner(self):
+        name = [
+            '                            __                           ',
+            '    ____  __  ______  ___  / /_     ______________ _____ ',
+            '   / __ \\/ / / / __ \\/ _ \\/ __/    / ___/ ___/ __ `/ __ \\',
+            '  / /_/ / /_/ / / / /  __/ /_     (__  ) /__/ /_/ / / / /',
+            ' / .___/\\__, /_/ /_/\\___/\\__/____/____/\\___/\\__,_/_/ /_/ ',
+            '/_/    /____/              /_____/                       '
+        ]
+        logo = [
+            '⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀',
+            '⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⠀⠀⠀⠀⠀⠀',
+            '⠀⠀⠀⣠⣶⡿⣿⣿⡿⠋⠉⠀⠀⠉⠙⢿⣿⡿⣷⣦⡀⠀⠀⠀',
+            '⠀⢀⣼⠟⠁⢠⣿⠏⠀⠀⢠⣤⣤⡀⠀⠀⢻⣿⡀⠙⢿⣦⠀⠀',
+            '⣰⡟⠁⠀⠀⢸⣿⠀⠀⠀⢿⣿⣿⡟⠀⠀⠈⣿⡇⠀⠀⠙⣷⡄',
+            '⠈⠻⣦⣄⠀⠸⣿⣆⠀⠀⠀⠉⠉⠀⠀⠀⣸⣿⠃⢀⣤⣾⠟⠁',
+            '⠀⠀⠈⠻⣿⣶⣿⣿⣦⣄⠀⠀⠀⢀⣠⣾⣿⣿⣾⡿⠋⠁⠀⠀',
+            '⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠁⠀⠀⠀⠀⠀',
+            '⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀'
+        ]
+        # maybe 0.04 sleep
+        self.colorized_print(name, type_text='i')
+        self.colorized_print(logo, type_text='e')
 
     """
     Its the charge print with colorized format all the output of the program
     Args:
-        - text (str): the text to printed
+        - text_arr (arr or str): the text to printed
         - color (str): color to set text
         - style (str): format of the font (bold, italic, etc)
         - type_text (str): type of message, this have predefined style options [default=None]
@@ -37,21 +62,27 @@ class Visuals:
             - 'error' or 'e'
             - 'info' or 'i'
     Explanation:
-        If type_text its setted ignore the rest of the styles defined (style and color)
+        If text_arr is list this print all lines with the style setted, but if is str the 
+        method become this str in list. And if type_text its setted ignore the rest of the
+        styles defined (style and color)
     """
-    def colorized_print(self, text, style=None, color=None, type_text=None):
+    def colorized_print(self, text_arr, style=None, color=None, type_text=None):
+        if isinstance(text_arr, str):
+            text = text_arr
+            text_arr = list()
+            text_arr.append(text)
+
         if type_text is not None:
-            # in function of type add ([!], [?], [*])
             match type_text:
-                case 'warning': #| w':
+                case 'warning' | 'w':
                     type_text = Visuals.type_msg['warning']
-                case 'error': # | e':
+                case 'error' | 'e':
                     type_text = Visuals.type_msg['error']
-                case 'info': # | i':
+                case 'info' | 'i':
                     type_text = Visuals.type_msg['info']
-            for txt in text: 
+
+            for txt in text_arr:
                 self.console.print(txt, style=type_text)
             return
-
-        for txt in text:
+        for txt in text_arr:
             self.console.print(f'[{style} {color}]{txt}')
